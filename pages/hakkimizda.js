88489c830f555cbdd176ab78fb4acbc7410e72d3
aeboyaci <ahmeterenboyaci45@gmail.com>
2022-01-12 13:18:13 +0300

    gecmis etkinlikler kismi eklendi.

--- pages/hakkimizda.js
+++ pages/hakkimizda.js
@@ -85,6 +85,52 @@ const Hakkimizda = () => {
               </span>
             </span>
                     </p>
+                    <h2>Ge&ccedil;miş Etkinlikler</h2>
+                    <p>- ISITDTU CTF<br />
+                        - Siber G&uuml;venlik Sekt&ouml;r&uuml; Tanıtım Etkinliği<br />
+                        - Sosyal Etkinlik (Final sonrası bowling)<br />
+                        - Siber G&uuml;venlik Yaz Kampı Ev Sahipliği<br />
+                        - Sosyal Etkinlik (YUSiber ile birlikte Anıtkabir ziyareti)<br />
+                        - Sosyal Etkinlik (Akşam yemeği)<br />
+                        - Bilgi Işıktır! Tanışma Toplantısı<br />
+                        - Network Eğitimleri (Ders serisi olarak 4 hafta ger&ccedil;ekleşti.)<br />
+                        - Sosyal Etklinkik (Zerodays belgeseli)<br />
+                        - Sosyal Etkinlik (Piknik)<br />
+                        - Red Team Etkinliği (Ofansif G&uuml;venlik)<br />
+                        - &Ouml;d&uuml;l Avcısı Geliyor! Bug Bounty Eğitimi<br />
+                        - Sosyal Etkinlik (Teknoloji Sohbetleri Etkinliğine Katılım)<br />
+                        - Exploit Development Eğitimi<br />
+                        - İleri Seviye Kriptoloji Eğitimi<br />
+                        - STM CTF<br />
+                        - Y&ouml;netim Kurulu Toplantısı ve Sosyal Kahve Etkinliği<br />
+                        - Sosyal Etkinlik (Snowden belgeseli)<br />
+                        - CTF Workshop<br />
+                        - CTF Workshop<br />
+                        - Tersine M&uuml;hendislik Eğitimi<br />
+                        - Internet of Things<br />
+                        - Blue Team Etkinliği (Defansif G&uuml;venlik)<br />
+                        - Sosyal Etkinlik (Anıtkabir ziyareti)<br />
+                        - Sosyal Etkinlik (AO&Ccedil; hatıra ormanı fidan dikme)<br />
+                        - RITSEC CTF<br />
+                        - Zararlı Yazılım Analizi ve Tersine M&uuml;hendislik &Ccedil;alıştayı<br />
+                        - BTK Siber Operasyonlar Eğitimi<br />
+                        - HAVELSAN SIEM/SOC Eğitimi<br />
+                        - Siber G&uuml;venlik Sekt&ouml;r&uuml; Hakkında Tea Talk<br />
+                        - Siber Tehdit İstihbaratı ve APT Grupları Etkinliği<br />
+                        - Wikipedia &Ouml;zg&uuml;r Ansiklopedi Etkinliği<br />
+                        - Adli Bilişim Eğitimi<br />
+                        - Practical Hacking Workshop<br />
+                        - Priviahub ve Hackthebox Platformları &Uuml;zerinden Uygulamalı Sızma Testi Etkinliği<br />
+                        - BTK Hack Nights Etkinlikleri (Katılım)<br />
+                        - Sosyal Etkinlik (&Uuml;ye buluşması)<br />
+                        - NATO 2021 Lockedshields Hazırlık Eğitimleri<br />
+                        - T&uuml;rk Telekom Siber G&uuml;venlik Kampı ve CTF (Online katılım)<br />
+                        - Siber G&uuml;venlik Haftası (Katılım)<br />
+                        - Turkcell Unibounty İşbirliği<br />
+                        - Career Lead Etkinliği (Katılım)</p>
+
+                    <p>Listede yer almayan ancak Covid-19 d&ouml;neminde bireysel ve toplu olarak katılım sağlanan bir&ccedil;ok etkinlik ve eğitim bulunmaktadır.</p>
+
                 </Container>
             </MainLayout>
         </React.Fragment>
