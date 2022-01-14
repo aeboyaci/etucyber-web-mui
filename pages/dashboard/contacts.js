b0a86c39d04ea1affc0021fd93b478a22739871b
Ahmet Eren BOYACI <ahmeterenboyaci45@gmail.com>
2022-01-14 20:09:51 +0000

    typo fix

--- pages/dashboard/contacts.js
+++ pages/dashboard/contacts.js
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import ContactMessage from "../../components/ContactMessage";
+import DashboardLayout from "../../components/DashboardLayout";
 
 const Contacts = () => {
     const [messages, setMessages] = useState(null);
 
     useEffect(async () => {
-        const response = await fetch("https://api.etucyber.com/api/contacts", {
+        const response = await fetch("https://api.etucyber.com/api/contact", {
             method: "GET",
             headers: {
                 'Accept': 'application/json',
@@ -19,24 +20,26 @@ const Contacts = () => {
     }, []);
 
     return (
-        <React.Fragment>
-            <div className="row">
-                <div className="col col-md-8 mx-auto">
-                    <h2>İletişim Mesajları</h2>
-                    {messages &&
-                    messages.map((msg, idx) => {
-                        return (
-                            <ContactMessage
-                                firstName={msg.firstName}
-                                lastName={msg.lastName}
-                                message={msg.message}
-                                email={msg.email}
-                            />
-                        );
-                    })}
+        <DashboardLayout>
+            <React.Fragment>
+                <div className="row">
+                    <div className="col col-md-8 mx-auto">
+                        <h2>İletişim Mesajları</h2>
+                        {messages &&
+                        messages.map((msg, idx) => {
+                            return (
+                                <ContactMessage
+                                    firstName={msg.firstName}
+                                    lastName={msg.lastName}
+                                    message={msg.message}
+                                    email={msg.email}
+                                />
+                            );
+                        })}
+                    </div>
                 </div>
-            </div>
-        </React.Fragment>
+            </React.Fragment>
+        </DashboardLayout>
     );
 };
 
